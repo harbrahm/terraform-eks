8a18d18f9f47e0ec72a333592711d61ba4ba138f
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 12:41:40 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-  cluster-name       = "${var.cluster-name}"
-  k8s-version        = "${var.k8s-version}"
-  aws-region         = "${var.aws-region}"
-  node-instance-type = "${var.node-instance-type}"
-  desired-capacity   = "${var.desired-capacity}"
-  max-size           = "${var.max-size}"
-  min-size           = "${var.min-size}"
-  vpc-subnet-cidr    = "${var.vpc-subnet-cidr}"
-
