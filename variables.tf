a5f018d2f0dca3bc27acf841eca4a2cf98318aae
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 12:43:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 # Variables Configuration
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 variable "cluster-name" {
   default     = "DOTS_cluster"
@@ -48,32 +50,3 @@ variable "min-size" {
   description = "Autoscaling Minimum node capacity"
 }
 
-variable "aws_vpc" {
-  default     = "vpc-07555e2e264da3d32"
-  type        = "string"
-  description = "VPC"
-}
-
-variable "aws_subnet" {
-  default     = "subnet-08840e2d912e51114"
-  type        = "string"
-  description = "subnet"
-}
-
-variable "aws_internet_gateway" {
-  default     = "igw-0b06d9c720dbbaa4f"
-  type        = "string"
-  description = "internet gateway"
-}
-
-variable "aws_route_table" {
-  default     = "rtb-0d666a4f9da483180"
-  type        = "string"
-  description = "route table"
-}
-
-variable "aws_route_table_association" {
-  default     = "rtbassoc-08ed38f552c26b2f2"
-  type        = "string"
-  description = "route table association"
-}
