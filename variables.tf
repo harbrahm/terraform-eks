82b0cbb164fc61ed8b04f0c4e90d5b6d190a961c
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 15:45:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
+varaible "vpc_id" {}  
+varaible "aws_subnet" {}
+variable "aws_internet_gateway " {}
+varaiable "aws_route_table " {}
+varaiable "aws_route_table_association" {}
+
+
+
 variable "cluster-name" {
   default     = "DOTS_cluster"
   type        = "string"
@@ -49,24 +57,4 @@ variable "min-size" {
   type        = "string"
   description = "Autoscaling Minimum node capacity"
 }
-
-data "terraform_remote_state" "static" {
- backend     = "s3"
-
- config {
-   bucket = "terraform-state-remote-storages"
-   key    = "terraform/dev"
-   region = "us-east-1"
- }
-}
-
-
- vpc_id  = "${data.terraform_remote_state.static.vpc_id}"
- aws_subnet  = "${data.terraform_remote_state.static.aws_subnet}"
- aws_internet_gateway  = "${data.terraform_remote_state.static.aws_internet_gateway}"
- aws_route_table  = "${data.terraform_remote_state.static.aws_route_table}"
- aws_route_table_association  = "${data.terraform_remote_state.static.aws_route_table_association}"
-
-
-
 
