b09382777ad9ea48bf9cf5d2c7e06868df0b7925
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 12:39:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,9 @@
 # Provider Configuration
 
 provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region = "${var.aws-region}"
 }
 
-# Using these data sources allows the configuration to be
-# generic for any region.
-data "aws_region" "current" {}
 
-data "aws_availability_zones" "available" {}
-
-provider "http" {}
