8ac2733ca5f9f6d9e87b2314cf2478765302f5c8
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 13:10:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -50,3 +50,27 @@ variable "min-size" {
   description = "Autoscaling Minimum node capacity"
 }
 
+data "terraform_remote_state" "static" {
+ backend     = "s3"
+
+ config {
+   bucket = "terraform-state-remote-storages"
+   key    = "terraform/dev"
+   region = "us-east-1"
+ }
+}
+
+module "app" {
+ source  = "modules/blue-green"
+ name    = "app"
+ vpc_id  = "${data.terraform_remote_state.static.vpc_id}"
+ aws_subnet  = "${data.terraform_remote_state.static.aws_subnet}"
+ aws_internet_gateway  = "${data.terraform_remote_state.static.aws_internet_gateway}"
+ aws_route_table  = "${data.terraform_remote_state.static.aws_route_table}"
+ aws_route_table_association  = "${data.terraform_remote_state.static.aws_route_table_association}"
+}
+
+
+
+
+
