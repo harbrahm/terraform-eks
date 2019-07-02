7fd944be2837fab126555808f45d86d340cb98bf
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-07-02 14:59:44 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -68,7 +68,8 @@ resource "aws_security_group_rule" "cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.cluster.id}"  
+  security_group_id        = "${aws_security_group.cluster.id}" 
+  source_security_group_id = "${aws_security_group.node.id}"
   to_port                  = 443
   type                     = "ingress"
 }
